af2549f3d7f06f3068b16f614fd3473725aa73ab
cdwcgt <cdwcgt@cdwcgt.top>
2023-09-18 23:18:26 +0900

    remove grid container

--- MatchHelperGUI.Game/MainScreen.cs
+++ MatchHelperGUI.Game/MainScreen.cs
@@ -1,9 +1,11 @@
 using MatchHelperGUI.Game.Chat;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Audio;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Screens;
 using osuTK;
 
@@ -17,32 +19,40 @@ namespace MatchHelperGUI.Game
         [BackgroundDependencyLoader]
         private void load()
         {
-            InternalChild = new FillFlowContainer
+            InternalChild = new Container
             {
                 RelativeSizeAxes = Axes.Both,
-                Direction = FillDirection.Vertical,
                 Margin = new MarginPadding(10),
-                Spacing = new Vector2(0, 5),
                 Children = new Drawable[]
                 {
                     infoText = new SpriteText { Text = "未连接", Font = OsuFont.GetFont().With(size: 20) },
-                    new GridContainer
+                    new Container
                     {
-                        RelativeSizeAxes = Axes.Both,
-                        ColumnDimensions = new[] { new Dimension(GridSizeMode.Relative, 0.6f) },
-                        Content = new[]
+                        Padding = new MarginPadding
                         {
-                            new Drawable[]
+                            Right = 200
+                        },
+                        RelativeSizeAxes = Axes.Both,
+                        Children = new Drawable[]
+                        {
+                            chat = new ChatDisplay
                             {
-                                chat = new ChatDisplay
-                                {
-                                    RelativeSizeAxes = Axes.Both,
-                                },
-                                new Box
-                                {
-                                    RelativeSizeAxes = Axes.Both,
-                                    Colour = Colour4.Aqua
-                                }
+                                RelativeSizeAxes = Axes.Both,
+                            },
+                        }
+                    },
+                    new Container
+                    {
+                        Anchor = Anchor.TopRight,
+                        Origin = Anchor.TopRight,
+                        Width = 200,
+                        RelativeSizeAxes = Axes.Y,
+                        Children = new Drawable[]
+                        {
+                            new Box
+                            {
+                                RelativeSizeAxes = Axes.Both,
+                                Colour = Colour4.Gray,
                             }
                         }
                     }
